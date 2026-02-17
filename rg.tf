59adcf42d7e23f49f36170bcf259ca86fd5b954f
murlidharkhamitkar <82697237+murlidharkhamitkar@users.noreply.github.com>
2026-02-17 18:47:33 +0530

    Add Terraform configuration for Azure resource group

--- rg.tf
+++ rg.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.60.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "dev-rg" {
+  name     = var.az_rg_name
+  location = "var.az_location_name"
+}
